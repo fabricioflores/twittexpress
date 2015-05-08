8f79458740c5ed73c023fb81d00fdd5d42333d59
Patricio Valarezo <patovala@pupilabox.net.ec>
2015-05-08 12:04:48 -0500

    new layout and fixes starting with yeoman

---
+++ client/app/tweetservice/tweetservice.service.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('twittexpressApp')
+.service('tweetservice', function ($http) {
+
+    return {
+        asyncSearch: function(query, since) {	
+            var data = {query: query};				
+
+            var queryUrl = '/search';
+            var promise = $http.post(queryUrl, data).then(function (response) {						
+                return response;
+            });
+            return promise;
+        }				
+    };
+});
---
+++ client/app/tweetservice/tweetservice.service.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('Service: tweetservice', function () {
+
+  // load the service's module
+  beforeEach(module('twittexpressApp'));
+
+  // instantiate service
+  var tweetservice, $httpBackend;
+  beforeEach(inject(function (_tweetservice_, _$httpBackend_) {
+    tweetservice = _tweetservice_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  afterEach(function () {
+      $httpBackend.verifyNoOutstandingRequest();
+      $httpBackend.verifyNoOutstandingExpectation();
+  });
+  /*
+   * This service should poll the server each n time to check
+   * if it has new twitts for today
+   * */
+  it('should poll a server', function () {
+    var ts;
+    tweetservice.asyncSearch('algo').then(
+      function(response){
+        ts = response.data;
+      }
+    );
+    $httpBackend.expectGET('/search?hashtag=algo')
+          .respond(200, []);
+    $httpBackend.flush();
+
+    expect(ts.length).toBe(0);
+  });
+
+});
