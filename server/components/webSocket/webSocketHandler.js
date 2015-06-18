144c7969044a2d7d040556c2b9e84f08452e110d
Patricio Valarezo <patovala@pupilabox.net.ec>
2015-06-17 21:15:21 -0500

    remove debug logs

--- server/components/webSocket/webSocketHandler.js
+++ server/components/webSocket/webSocketHandler.js
@@ -40,8 +40,6 @@ module.exports = function(server){
         loadTweets(tweetlog, function(_tweets_){
             tweets = _tweets_;
 
-            console.log('DEBUG:', config.query);
-
             var stream = T.stream('statuses/filter', {
                 track: config.query || '@patovala'
             });
