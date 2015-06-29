98eeb5765163e536f34285a4ea153488f53cc36b
Sjlmejia <jlmejiam@unl.edu.ec>
2015-06-29 16:14:24 -0500

    test checking if addWhiteListuser works

--- server/api/config/config.controller.js
+++ server/api/config/config.controller.js
@@ -1,15 +1,35 @@
 'use strict';
-
 var config = require('../../config/environment/query.json');
 var fs = require('fs');
-
+var wsh = require('../../components/webSocket/webSocketHandler.js');
 var _ = require('lodash');
-
 // Get list of configs
 exports.index = function(req, res) {
   var resp = [];
-  if (req.method == 'POST') {
-    var q = req.body;
+  var q = req.body;
+  if (req.method === 'POST') {
+    if(req.query.acl === 'whitelist' ){
+      if (_.contains(config.users.wList, q.user)){
+        fs.writeFile('./server/config/environment/query.json', JSON.stringify(config), function(err){
+        if(err){
+        console.log('murio ', err);
+        }
+        //res.status(200).send({resp: 'query updated'});
+        resp = {resp: 'user added'};
+        res.json(resp);
+        });
+      }else{
+        config.users.wList.push(q.user);
+        fs.writeFile('./server/config/environment/query.json', JSON.stringify(config), function(err){
+        if(err){
+        console.log('murio ', err);
+      }
+      //res.status(200).send({resp: 'query updated'});
+      resp = {resp: 'user added'};
+      res.json(resp);
+    });
+    }
+    }else{
     config.query = q.query;
     fs.writeFile('./server/config/environment/query.json', JSON.stringify(config), function(err){
       if(err){
@@ -19,5 +39,6 @@ exports.index = function(req, res) {
       resp = {resp: 'query updated'};
       res.json(resp);
     });
+  }
   }
 };
